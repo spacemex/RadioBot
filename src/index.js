9cf5d66cbeaacfaa3e09f28453f03f1bfb7fd777
Deven Shona <51067067+spacemex@users.noreply.github.com>
2024-11-28 21:09:59 -0600

    port

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const voiceChannelId = process.env.VOICE_CHANNEL_ID;
 const announceChannelId = process.env.ANNOUNCE_CHANNEL_ID;
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8138;
 
 // Serve the static files from the public directory
 app.use(express.static('public'));
